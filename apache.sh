116da5f67eb27a08c2df9e9dac007d26dd4d3eb0
croberts1313 <croberts1313@bellsouth.net>
2017-11-27 13:47:06 -0600

    Updated

--- apache.sh
+++ apache.sh
@@ -2,13 +2,11 @@
 
 sudo yum install -y httpd
 
-groupadd www
-usermod -a -G www ec2-user
+sudo groupadd www
+sudo usermod -a -G www ec2-user
 
-chown -R root:www /var/www
-chmod 2775 /var/www
+sudo chown -R root:www /var/www
+sudo chmod 2775 /var/www
 
-find /var/www -type d -exec chmod 2775 {} +
-find /var/www -type f -exec chmod 0664 {} +
-
-
+sudo find /var/www -type d -exec chmod 2775 {} +
+sudo find /var/www -type f -exec chmod 0664 {} +
